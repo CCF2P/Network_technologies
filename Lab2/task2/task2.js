0fb1d39a5f865cbcd7657c74a4b09f40745cba92
ccf2p <yura.smolyaninov.01@mail.ru>
2023-05-01 18:06:33 +1000

    Сделал ИДЗ

--- Lab2/task2/task2.js
+++ Lab2/task2/task2.js
@@ -1,7 +1,7 @@
 let svg = d3.select(".graphic")
             .append("svg")
-            .attr("width", "640")
-            .attr("height", "480");
+            .attr("width", "600")
+            .attr("height", "600");
 let centerX = svg.attr("width") / 2;
 let centerY = svg.attr("height") / 2;
 
@@ -9,51 +9,27 @@ let amplX = centerX * 0.9;
 let amplY = centerY * 0.9;
 let W1 = 2;
 let W2 = 1;
-let PHASE = 40;
-let RADIUS = 5;
+let PHASE = 40.06;
+let RADIUS = 7;
+let r;
 
 
-function setCircle(x, y) {
-    svg.append("circle")
-        .attr("cx", x)
-        .attr("cy", y)
-        .attr("r", RADIUS);
-}
-
-
-function Draw(count) {
-    if (count.length === 0) {
-        return;
-    }
-    if (isNaN(parseFloat(count)) || !isFinite(count) || count < 1) {
-        alert("Количество должно быть числом большем или равным 1.");
-        return;
-    }
-    count = Number(count);
-
+function Draw() {
     if (svg.selectAll("circle").length !== 0) {
         svg.selectAll("circle").remove();
     }
 
-    let c = 0;
-    let phaseAgain = W1 * W2 * Math.PI * 2.0;
-    for (let phase = 0; phase < phaseAgain; phase += Math.PI / 100) {
-        setCircle(centerX + Math.cos(W1 * phase) * amplX,
-            centerY + Math.cos(W2 * phase + PHASE) * amplY);
-        c++;
-        sleep(1000);
-        if (c === 2) {
-            document.querySelectorAll("circle")[0].remove();
-            c = 0;
-        }
-    }
+    svg.append("circle")
+        .attr("cx", centerX + Math.cos(W1 * 0) * amplX)
+        .attr("cy", centerY + Math.cos(W2 * 0 + PHASE) * amplY)
+        .attr("r", RADIUS);
+
+    let phase = 0;
+    r = setInterval(() => {
+        phase += Math.PI / 100
+        svg.select("circle")
+            .attr("cx", centerX + Math.cos(W1 * phase) * amplX)
+            .attr("cy", centerY + Math.cos(W2 * phase + PHASE) * amplY)
+            .attr("r", RADIUS);
+    }, 50);
 }
-
-
-function sleep(milliseconds) {
-    const date = Date.now();
-    let currentDate = null;
-    do {
-      currentDate = Date.now();
-    } while (currentDate - date < milliseconds);
-  }
